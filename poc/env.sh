a080df44453e207c126c1e7e4463c628f82ec33e
Filip Rembiałkowski <frembialkowski@clickup.com>
2024-08-08 13:28:06 +0200

    add haproxy, update configs

--- poc/env.sh
+++ poc/env.sh
@@ -1,6 +1,10 @@
-PG_URI_MASTER="host=127.0.0.1 dbname=postgres user=postgres password=postgres port=5432"
-PG_URI_RO="host=127.0.0.1 dbname=clickup user=app1_read password=app1_read port=5433"
-PG_URI_RW="host=127.0.0.1 dbname=clickup user=app1_write password=app1_write port=5432"
-PG_URI_RO_POOL="host=127.0.0.1 dbname=app1_read user=app1_read password=app1_read port=6432"
-PG_URI_RW_POOL="host=127.0.0.1 dbname=app1_write user=app1_write password=app1_write port=6432"
-PG_URI_PGCAT_ADMIN="host=127.0.0.1 dbname=pgcat user=pgcat password=pgcat port=6432"
+PG_URI_MASTER="host=127.0.0.1 dbname=postgres user=postgres password=postgres port=5432 target_session_attrs=read-write"
+
+PG_URI_RO="host=127.0.0.1,127.0.0.1,127.0.0.1 dbname=clickup user=app1_read password=app1_read port=5433,5434,5435"
+PG_URI_RO_POOL="host=127.0.0.1,127.0.0.1 dbname=app1_read user=app1_read password=app1_read port=6432,6433"
+PG_URI_RO_POOL_HA="host=127.0.0.1 dbname=app1_read user=app1_read password=app1_read port=7432"
+
+PG_URI_RW="host=127.0.0.1 dbname=clickup user=app1_write password=app1_write port=5432 target_session_attrs=read-write"
+PG_URI_RW_POOL="host=127.0.0.1,127.0.0.1 dbname=app1_write user=app1_write password=app1_write port=6432,6433 target_session_attrs=read-write"
+
+PG_URI_PGCAT_ADMIN="host=127.0.0.1,127.0.0.1 dbname=pgcat user=pgcat password=pgcat port=6432,6433"
--- poc/tester.sh
+++ poc/tester.sh
@@ -1,34 +1,69 @@
 #!/usr/bin/env bash
+set -eEo pipefail
 
-LIVENESS_TIMEOUT=10  # seconds
+# load PG_URI* env vars from env.sh
+{ set -a && source $(dirname -- $0)/env.sh && set +a; } || return
 
 main () {
-    { set -a && source $(dirname -- $0)/env.sh && set +a; } || return
+    workmode=$1
+    case $workmode in
+        liveness)
+            liveness
+            ;;
+        tps)
+            tps
+            ;;
+        *)
+            echo "Usage: $0 ( liveness | tps )"
+    esac
+}
+
+tps () {
+    # Run simple bench on tested endpoints and show TPS value 
+    delay=3.0
+    while true; do
+        echo -n "$(date -u) read-only: "
+        check_tps "$PG_URI_RO_POOL" select-only 3
+        echo -n ", read-write: "
+        check_tps "$PG_URI_RW_POOL" simple-update 1
+        echo "."
+        sleep $delay
+    done
+}
+
+liveness () {
+    # Show endpoint status and response time
     i=0
-    delay=1.0
+    delay=0.75
     show_dbs_every=10
     show_pools_every=60
     while true; do
-        let i++
-        echo -n "$(date -u) $i readonly: "
-        mydbtest "$PG_URI_RO_POOL"
-        echo -n ", read/write: "
-        mydbtest "$PG_URI_RW_POOL"
+        i=$((i + 1))
+        echo -n "$(date -u) read-only: "
+        check_liveness "$PG_URI_RO_POOL" "select 1"
+        echo -n ", read-only HA: "
+        check_liveness "$PG_URI_RO_POOL_HA" "select 1"
+        echo -n ", read-write: "
+        check_liveness "$PG_URI_RW_POOL" "update pgbench_branches set filler='$0' where bid = 1"
+        echo -n ", pgcat-admin: "
+        check_liveness "$PG_URI_PGCAT_ADMIN" "show version"
         echo "."
         # from time to time, show pooler stats
         if [ $(( $i % $show_dbs_every )) -eq 0 ]; then
-            psql -X "$PG_URI_PGCAT_ADMIN" -c "show databases" | cat
+            psql -X "$PG_URI_PGCAT_ADMIN" -c "show databases" 2>/dev/null | cat || true
         fi
         if [ $(( $i % $show_pools_every )) -eq 0 ]; then
-            psql -X "$PG_URI_PGCAT_ADMIN" -c "show pools" | cat
+            psql -X "$PG_URI_PGCAT_ADMIN" -c "show pools" 2>/dev/null | cat || true
         fi
         sleep $delay
     done
 }
 
-mydbtest () {
+check_liveness () {
+    uri="$1"
+    query="$2"
     time0=$(date +%s%3N)  # ms
-    if timeout ${LIVENESS_TIMEOUT-10} psql -XqAtc ";" "$@" &>/dev/null; then
+    if timeout 10 psql "$uri" -XqAtc "$query" &>/dev/null; then
         status=OK
     else
         status=FAIL
@@ -36,6 +71,25 @@ mydbtest () {
     time1=$(date +%s%3N)  # ms
     timedelta=$((time1 - time0))
     echo -n "${status} (${timedelta} ms)"
+}
+
+check_tps () {
+    uri="$1"
+    builtin="${2:-select-only}"
+    scale="${3:-10}"
+    time0=$(date +%s%3N)  # ms
+    tmpfile=$(mktemp)
+    if timeout $((scale * 2)) pgbench "$uri" -n -b "$builtin" -c "$scale" -T "$scale" &> "$tmpfile"; then
+        status=OK
+        tps=$(awk '/^tps/{print $3; exit}' < "$tmpfile" | cut -d. -f1)
+    else
+        status=FAIL
+        tps=0.0
+    fi
+    time1=$(date +%s%3N)  # ms
+    timedelta=$((time1 - time0))
+    rm -f "$tmpfile"
+    echo -n "$status ($tps tps, $timedelta ms)"
 }
 
 main "$@"
